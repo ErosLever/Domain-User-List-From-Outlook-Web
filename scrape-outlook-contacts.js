f8ad6e3fd4435da842ca6a6be79eb02015eb6903
Eros Lever <ErosLever@users.noreply.github.com>
2026-02-06 16:42:28 +0100

    fixed syntax

--- scrape-outlook-contacts.js
+++ scrape-outlook-contacts.js
@@ -152,7 +152,7 @@ async function fetchAllItems() {
         }
     }
 
-    let export_filename = `office365_export_${tenant_id}_${new Date().toISOString().replace(/\D/g,'')}.csv`
+    let export_filename = `office365_export_${tenant_id}_${new Date().toISOString().replace(/\D/g,'')}.csv`;
 
     // Download database as a .csv file
     console.debug('Converting user array to csv...')
